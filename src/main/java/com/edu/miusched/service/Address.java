2f0f83a31c504ca9526bb3f95c31625b4ab1bc51
Isam123 <ime432@gmail.com>
2020-05-05 08:08:56 -0500

    Class and stylesheet commit

---
+++ src/main/java/com/edu/miusched/service/Address.java
@@ -0,0 +1,4 @@
+package com.edu.miusched.service;
+
+public interface Address {
+}
---
+++ src/main/java/com/edu/miusched/service/impl/Adressimpl.java
@@ -0,0 +1,6 @@
+package com.edu.miusched.service.impl;
+
+import com.edu.miusched.service.Address;
+
+public class Adressimpl implements Address {
+}
