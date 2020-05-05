3f0137d67a3155c15b9c24d9d7485d40f8409013
Amanuel Negussie Kihshen <62534492+akihshen@users.noreply.github.com>
2020-05-05 16:35:23 -0500

    added Admin entity

--- src/main/java/com/edu/miusched/domain/Admin.java
+++ src/main/java/com/edu/miusched/domain/Admin.java
@@ -1,4 +1,23 @@
 package com.edu.miusched.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Admin {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    @NotEmpty
+    @OneToOne
+    private Address address;
+    @OneToOne
+    private Account account;
 }
