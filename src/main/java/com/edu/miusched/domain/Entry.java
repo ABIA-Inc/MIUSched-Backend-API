a57a69a5b14bc3873520f865bec9639c2223a34e
Biruk Bekele <biruk.bekele@gmail.com>
2020-05-07 01:19:17 -0500

    update entry serivces, controller

--- src/main/java/com/edu/miusched/domain/Entry.java
+++ src/main/java/com/edu/miusched/domain/Entry.java
@@ -4,9 +4,11 @@ package com.edu.miusched.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,8 +27,10 @@ public class Entry {
     private String  entryName;
     private int FPPNum;
     private int MPPNum;
-    private Date StartDate;
-    private Date EndDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate StartDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate EndDate;
     private EntryType entryType;
     @OneToMany(cascade = CascadeType.ALL,mappedBy="entry")
     private List<Student> students = new ArrayList<>();
--- src/main/java/com/edu/miusched/service/impl/EntryServiceImpl.java
+++ src/main/java/com/edu/miusched/service/impl/EntryServiceImpl.java
@@ -1,4 +1,39 @@
 package com.edu.miusched.service.impl;
 
-public class EntryServiceImpl {
+
+import com.edu.miusched.dao.EntryDao;
+import com.edu.miusched.domain.Entry;
+import com.edu.miusched.service.EntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EntryServiceImpl  implements EntryService {
+
+     @Autowired
+    EntryDao entryDao;
+
+    @Override
+    public List<Entry> getAllEntries() {
+        return entryDao.findAll();
+    }
+
+    @Override
+    public void save(Entry entry) {
+        entryDao.save(entry);
+
+    }
+
+    @Override
+    public void deleteEntry(Long id) {
+        entryDao.deleteById(id);
+
+    }
+
+    @Override
+    public Entry findByEntryId(Long id) {
+        return entryDao.findEntryById(id);
+    }
 }
