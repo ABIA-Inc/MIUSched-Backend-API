0d8f1f4fce6641e33e40271c81730306940cade7
Amanuel Negussie Kihshen <62534492+akihshen@users.noreply.github.com>
2020-05-11 22:17:48 -0500

    modified Block entity, service, contr and view

--- src/main/java/com/edu/miusched/service/impl/BlockServiceImpl.java
+++ src/main/java/com/edu/miusched/service/impl/BlockServiceImpl.java
@@ -1,4 +1,51 @@
 package com.edu.miusched.service.impl;
 
-public class BlockServiceImpl {
+import com.edu.miusched.dao.BlockDao;
+import com.edu.miusched.domain.Block;
+import com.edu.miusched.service.BlockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class BlockServiceImpl implements BlockService {
+    @Autowired
+    BlockDao blockDao;
+
+    @Override
+    public void save(Block block) {
+        blockDao.save(block);
+    }
+
+    @Override
+    public Block getBlockById(Long id) {
+        return blockDao.findBlockById(id);
+    }
+
+    @Override
+    public List<Block> getAllBlocks() {
+        return blockDao.findAll();
+    }
+
+    @Override
+    public Block getBlockByName(String blockName) {
+        return blockDao.findBlockByBlockName(blockName);
+    }
+
+    @Override
+    public Block getBlockByStartDate(LocalDate startDate) {
+        return blockDao.findBlockByStartDate(startDate);
+    }
+
+    @Override
+    public void deleteBlockById(Long id) {
+        blockDao.deleteById(id);
+    }
+
+    @Override
+    public void deleteBlockByName(String blockName) {
+        blockDao.deleteBlockByBlockName(blockName);
+    }
 }
